0581ee5f2801c9bab88b71098e39b39f78f5f646
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-11 17:01:08 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
- my_all?(collection)
+ def my_all?(collection)
   i = 0
   while i < collection.length
-    i = i + 1   
+    yield(collection[i])
+    i = i + 1
   end
-enddef
+end
